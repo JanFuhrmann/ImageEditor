df57c55ebcaf50c7eb6c0f054cd72d3069b3c653
Jan <st143351@stud.uni-stuttgart.de>
2020-12-19 23:38:14 +0100

    Minor changes in Start.java

--- src/Start.java
+++ src/Start.java
@@ -5,7 +5,7 @@ public class Start {
     public static void main(String[] args) {
         Structure frame = new Structure();
         frame.setVisible(true);
-        frame.setTitle("Bildbearbeitungsprogramm by Jan Fuhrmann");
+        frame.setTitle("Image editing program by Jan Fuhrmann");
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
     }
 
